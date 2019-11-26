238070ed1f72c83343e86a47983476c31b6d9d58
Valentine Voyen <valentine.voyen1@gmail.com>
2019-11-26 10:55:35 +0100

    added fecth api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 const App = () => {
   const APP_ID = '91a10345';
   const APP_KEY = 'a3a2dcc078ae587ce7ad48b9dd82a2d7';
-  const exampleReq ="https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}"
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    console.log(data);
+  };
 
   return(
     <div className='App'>
       <form className='search-form'>
         <input className='search-bar' type='text'/>
-        <button className='search-button' type='submit'>Search</button>
+        <button 
+          className='search-button' 
+          type='submit'
+        >
+          Search
+        </button>
       </form>
     </div>
   )
